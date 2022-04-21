ca4badcb4dda5f10a134f21e50023f6666d7b440
Comicnist099 <marco.ctu@hotmail.com>
2022-04-21 16:42:22 -0500

    alta noticias mejorado

---
+++ Dashboard/Modificar_Noticia.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Crear Noticias</title>
+
+
+<script>
+        function mialerta() {  
+            alert("Se ha creado la noticia");
+        } 
+    </script>
+
+<link rel="stylesheet" href="css/Modificar_Noticias.css">
+
+<?php include('./Templates/Nav_Bar.php')?>
+<body style="background-image:  url('./img/bg.jpg'); background-repeat: no-repeat;  background-size: cover;
+">
+<div class="Forms">
+<img src="./img/banner5.png" alt="Italian Trulli" style="align-items: center;">
+<h1>EDICION DE NOTICIA</h1>
+<h3>COSAS PRINCIPALES</h3>
+<h6>Categoría de color</h6>
+
+
+  <div class="custom-select" style="width:200px;">
+  <select id="Selectores_color"class="form-control"  >
+    <option value="0">Selecciona el color:</option>
+    <option value="1">Rojo</option>
+    <option value="2">Verde</option>
+    <option value="3">Azul</option>
+    <option value="4">Amarillo</option>
+    <option value="5">Negro</option>
+   
+  </select>
+</div>
+
+<h6>Titulo de la noticia</h6>
+<input class="form-control" type="text" placeholder="Noticia con un titulo emocionante"><br>
+<h6>Contenido</h6>
+
+<div class="Contenido">
+  
+<textarea class="form-control" placeholder="Colocar contenido de la noticia"></textarea> 
+</div>
+<br>
+
+
+
+ <h6>Palabras Claves</h6>
+
+
+<input class="form-control" type="text" placeholder="Fantasico, violin... etc"><br>
+
+<h6>Firma del Reportero</h6>
+
+<input class="form-control" type="text" placeholder="Coloca un alias o como quieres que te reconzcan"><br>
+
+<div class="form-control">
+      <label for="username">Fecha de la noticia</label>
+        <input type="date" placeholder="Fecha de la notica" id="date" size="50" class="form-control" min="1910-01-01" max="2020-01-01">
+    <i class="fas fa-check-circle"></i>
+      <i class="fas fa-exclamation-circle"></i>
+                           
+  </div>
+
+<h3>CATEGORIAS</h3>
+<div class="container">
+  <h6 class="page-header">Elige todas las categorias que quieras</h6>
+  <div class="row">
+    <div class="col-sm-3">
+      <select id="Selectores"class="form-control"  >
+        <option value="Deportes">Deportes</option>
+        <option value="Espectaculos">Espectaculos</option>
+        <option value="Monitos">Monitos</option>
+      </select>
+      <button class="btn-add">+</button>
+      <div class="li-container">
+            <ul>
+         
+            </ul>
+        </div>
+        <div class="empty">
+            <p>No haz añadido elementos a la lista aún.</p>
+        </div>
+    </div>
+  </div>
+
+  <h3>AÑADE FOTOS O VIDEOS</h3>
+
+<input class="form-control" name="uploadedfile" type="file" />
+<input class="form-control" name="uploadedfile" type="file" />
+<input class="form-control" name="uploadedfile" type="file" />
+<input class="form-control" name="uploadedfile" type="file" />
+<br>
+<br>
+
+<button href="index.php"  onclick="mialerta();"   type="button" class="btn btn-dark">Aceptar noticia</button>
+</div>
+</div>
+
+<script src="js/etiquetas.js"> </script>
+<?php include('./Templates/Footer.php')?>
+
+
+
+</body>
+</html>
---
+++ Dashboard/noticia_revision.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+<link rel="stylesheet" href="css/style_noticia_revision.css">
+<script src="js/jquery-3.6.0.min.js"></script>
+
+<?php include('./Templates/Nav_bar.php') ?>
+
+<script>
+        function mialerta() {  
+            alert("Bienvenido, Entra y conoce un mundo nuevo de noticias");
+        } 
+    </script>
+<body style="background-image:  url('./img/bg.jpg'); background-size: 100% 100%; 
+background-attachment: fixed;
+
+">
+  <br>
+  <div class="ContenedorBus">
+  <div class="Titulo2">
+  <br>
+  <h1>Noticias en revisión</h1>
+  </div>
+<div class="Titulo">
+  <br>
+<h5>Buscadores </h5>
+
+<input class="form-control" type="text" placeholder="Palabras Claves"><br>
+</div>
+<div id="reportrange" >
+    <i class="fa fa-calendar"></i>&nbsp;
+    <span></span> <i class="fa fa-caret-down"></i>
+</div>
+</div>
+<script type="text/javascript">
+$(function() {
+
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+           'Hoy': [moment(), moment()],
+           'ayer': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Hace 7 días': [moment().subtract(6, 'days'), moment()],
+           'Hace 30 días': [moment().subtract(29, 'days'), moment()],
+           'Este Mes': [moment().startOf('month'), moment().endOf('month')],
+           'Ultimo mes': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
+
+});
+</script>
+
+  <section class="Reciente">
+    <h1>POPULARES</h1><br><br><br>
+    <div class=ConjuntoNoticias>
+      <span Class="Noticia1">SANTAS CACHUCHAS LO AGARRON CON EL TINNER Y RATA EN MANO</span>
+      <span Class="Noticia2">SANTAS CACHUCHAS LO AGARRON CON EL TINNER Y RATA EN MANO</span>
+      <span Class="Noticia3">SANTAS CACHUCHAS LO AGARRON CON EL TINNER Y RATA EN MANO</span>
+    </div>
+
+  </section>
+
+  
+
+
+  <div id="sidebar">
+    <div class="custom">
+      <div class="toggle-btn">
+        <span>&#9776;</span>
+      </div>
+    </div>
+
+    <h1 >SECCIONES</h1><br><br>
+    <a>
+      <div class="Logo_Secciones">
+        <img src="img/ranita.png" style="height:150px" alt="logotipo">
+    </a>
+  </div>
+
+  <div class="custom">
+    <div id="front_videos">
+      <div class="large-2">
+        <a>Deportes <br> </a>
+        <a>Espectaculos <br> </a>
+        <a>Deportes <br> </a>
+        <a>Espectaculos <br> </a>
+        <a>Deportes <br> </a>
+        <a>Espectaculos <br> </a>
+        <a>Deportes <br> </a>
+        <a>Espectaculos <br> </a>
+        <a>Deportes <br> </a>
+        <a>Espectaculos <br> </a>
+        <a>Deportes <br> </a>
+        <a>Espectaculos <br> </a>
+        <a>Deportes <br> </a>
+        <a>Espectaculos <br> </a>
+
+
+        <div class="force-overflow"></div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+ 
+
+
+  <div class="Noticia">
+    <div class="ConjuntoImg">
+      <a class="imagenPublic"><img src="img/descarga.jpg" alt="logotipo"></a>
+
+    </div>
+    <section class="Publicaciones">
+      <div class="Casilla">
+        <div class=CasillaFecha>
+          <a style="font-weight:700">Fecha de Publicacion: </a><a>28/21/2022</a> <br>
+          <a style="font-weight:700">Localización: </a><a>Colonia, </a><a>Ciudad, </a><a>Pais </a><br>
+        </div>
+        <div class="large-4">
+          <span>Deportes</span>
+          <span>Deportes</span>
+          <span>Deportes</span>
+        </div>
+        <div class="CasillaTitulo">
+          <span > TUSA DOS CONFIRMADA NO PUEDE SER INCREIBLE!!!!!!!!!</span><br>
+
+        </div>
+        <div class="large-3" style="font-weight:700">
+          <a>Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora
+            tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser
+            el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela
+            Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene
+            una gran secuela</a>
+        </div>
+
+        <br><br><br>
+      </div>
+    </section>
+  </div>
+
+  <div class="Noticia">
+    <div class="ConjuntoImg">
+      <a class="imagenPublic"><img src="img/descarga.jpg" alt="logotipo"></a>
+
+    </div>
+    <section class="Publicaciones">
+
+
+      <div class="Casilla">
+
+        <div class=CasillaFecha>
+
+          <a style="font-weight:700">Fecha de Publicacion: </a><a>28/21/2022</a> <br>
+          <a style="font-weight:700">Localización: </a><a>Colonia, </a><a>Ciudad, </a><a>Pais </a><br>
+
+        </div>
+        <div class="large-4">
+          <span>Deportes</span>
+          <span>Deportes</span>
+          <span>Deportes</span>
+
+        </div>
+
+
+        <div class="CasillaTitulo">
+          <span style=" font-weight:900"> TUSA DOS CONFIRMADA NO PUEDE SER INCREIBLE!!!!!!!!!</span><br>
+
+        </div>
+        <div class="large-3" style="font-weight:700">
+          <a>Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora
+            tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser
+            el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela
+            Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene
+            una gran secuela</a>
+        </div>
+
+        <br><br><br>
+
+      </div>
+    </section>
+  </div>
+  <div class="Noticia">
+    <div class="ConjuntoImg">
+      <a class="imagenPublic"><img src="img/descarga.jpg" alt="logotipo"></a>
+
+    </div>
+    <section class="Publicaciones">
+
+
+      <div class="Casilla">
+
+        <div class=CasillaFecha>
+
+          <a style="font-weight:700">Fecha de Publicacion: </a><a>28/21/2022</a> <br>
+          <a style="font-weight:700">Localización: </a><a>Colonia, </a><a>Ciudad, </a><a>Pais </a><br>
+
+        </div>
+        <div class="large-4">
+          <span>Deportes</span>
+          <span>Deportes</span>
+          <span>Deportes</span>
+
+        </div>
+
+
+        <div class="CasillaTitulo">
+          <span style=" font-weight:900"> TUSA DOS CONFIRMADA NO PUEDE SER INCREIBLE!!!!!!!!!</span><br>
+
+        </div>
+        <div class="large-3" style="font-weight:700">
+          <a>Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora
+            tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser
+            el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela
+            Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene
+            una gran secuela</a>
+        </div>
+
+        <br><br><br>
+
+      </div>
+    </section>
+  </div>
+  <div class="Noticia">
+    <div class="ConjuntoImg">
+      <a class="imagenPublic"><img src="img/descarga.jpg" alt="logotipo"></a>
+
+    </div>
+    <section class="Publicaciones">
+
+
+      <div class="Casilla">
+
+        <div class=CasillaFecha>
+
+          <a style="font-weight:700">Fecha de Publicacion: </a><a>28/21/2022</a> <br>
+          <a style="font-weight:700">Localización: </a><a>Colonia, </a><a>Ciudad, </a><a>Pais </a><br>
+
+        </div>
+        <div class="large-4">
+          <span>Deportes</span>
+          <span>Deportes</span>
+          <span>Deportes</span>
+
+        </div>
+
+
+        <div class="CasillaTitulo">
+          <span style=" font-weight:900"> TUSA DOS CONFIRMADA NO PUEDE SER INCREIBLE!!!!!!!!!</span><br>
+
+        </div>
+        <div class="large-3" style="font-weight:700">
+          <a>Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora
+            tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser
+            el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene una gran secuela
+            Escrible no puede ser el gran hit ahora tiene una gran secuela Escrible no puede ser el gran hit ahora tiene
+            una gran secuela</a>
+        </div>
+
+        <br><br><br>
+
+      </div>
+    </section>
+  </div>
+
+  <?php include('./Templates/Footer.php')?>
+
+ 
+  <script src="js/Sidebar.js"> </script>
+</body>
+
+</html>
